592e1975dbdb0e744686e85dfadbcfa81582b326
Yang <y2gcoder@gmail.com>
2021-01-10 23:23:45 +0900

    fix: add create table query in backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const app = express();
 //json 형태로 오는 요청의 본문을 해석해줄 수 있게 등록
 app.use(bodyParser.json());
 
-//테이블 생성하기
-// db.pool.query(`CREATE TABLE lists (
-//     id INTEGER AUTO_INCREMENT,
-//     value TEXT,
-//     PRIMARY KEY (id)
-// )`, (err, results, field) => {
-//     console.log('results', results);
-// });
+// 테이블 생성하기
+db.pool.query(`CREATE TABLE lists (
+    id INTEGER AUTO_INCREMENT,
+    value TEXT,
+    PRIMARY KEY (id)
+)`, (err, results, field) => {
+    console.log('results', results);
+});
 
 //DB list 테이블에 있는 모든 데이터를 프론트 서버에 보내주기
 app.get('/api/values', function(req, res) {
